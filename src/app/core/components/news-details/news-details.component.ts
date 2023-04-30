c97932028dc61bc554d66bb6fd81318fb3237876
EmanMohamed22 <124403280+EmanMohamed22@users.noreply.github.com>
2023-05-01 00:07:14 +0300

    news

---
+++ src/app/core/components/news-details/news-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-details',
+  templateUrl: './news-details.component.html',
+  styleUrls: ['./news-details.component.scss']
+})
+export class NewsDetailsComponent {
+
+}
