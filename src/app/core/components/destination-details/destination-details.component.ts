0dd9f7a2b163f4609e1a74cf0b87a1f50a6ae33c
Abdulrahman Samir <101456209+mythic357@users.noreply.github.com>
2023-05-25 17:38:03 +0300

    Update destination-details.component.ts

--- src/app/core/components/destination-details/destination-details.component.ts
+++ src/app/core/components/destination-details/destination-details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-destination-details',
   templateUrl: './destination-details.component.html',
-  styleUrls: ['./destination-details.component.scss']
+  styleUrls: ['./destination-details.component.scss'],
 })
-export class DestinationDetailsComponent {
+export class DestinationDetailsComponent implements OnInit {
+  destinationCountry:any = '' ;
+  constructor(private _ActivatedRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this._ActivatedRoute.paramMap.subscribe((params) => {
+      this.destinationCountry = params.get('dist');
+      console.log(this.destinationCountry);
+    });
+  }
 }
+  
